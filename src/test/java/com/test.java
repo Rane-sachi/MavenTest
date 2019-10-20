1f8303710322424b234dc8854e21df06d85ceeb0
Sachita <ranesachi@gmail.com>
2019-10-20 14:44:40 +0530

    addng fils

---
+++ src/test/java/com/test.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class test {
+	@Test
+	public void test() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Printing number "+i);
+		}
+		
+	}
+
+}
